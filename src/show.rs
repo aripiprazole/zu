bc7ee982965f6630b01f14544d21f4c2c3f68370
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-09-02 18:05:55 +0000

    feat(elab): add stub for show

--- src/show.rs
+++ src/show.rs
@@ -1,23 +1,25 @@
-use std::fmt::Debug;
-
 use crate::ast::Term;
 use crate::erase::Erased;
 use crate::erase::Lvl;
 use crate::nfe::Nfe;
 use crate::passes::elab::Elab;
+use crate::passes::elab::Quote;
 use crate::passes::elab::Value;
 
 /// The context we need to pretty print a type right now.
 ///
 /// It's a list of names, and the level of the last bound variable.
-pub struct Show {
-  pub level: Lvl,
-  pub names: Vec<String>,
-  pub term: Term<Erased>,
+struct Show {
+  lvl: Lvl,
+  names: Vec<String>,
+  term: Term<Erased>,
 }
 
-impl Debug for Show {
-  fn fmt(&self, _: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl Show {
+  fn build(self) -> Nfe {
+    let _ = self.lvl;
+    let _ = self.names;
+    let _ = self.term;
     todo!()
   }
 }
@@ -25,7 +27,13 @@ impl Debug for Show {
 impl Value {
   /// Show a value as a string. As the debruijin level 0 is the last bound variable, we need to
   /// pass the size of the environment.
-  pub fn show(&self, _: &Elab) -> Nfe {
-    todo!()
+  pub fn show(&self, elab: &Elab) -> Nfe {
+    let show = Show {
+      lvl: elab.lvl,
+      names: elab.types.clone().into_iter().map(|(k, _)| k).collect(),
+      term: self.clone().quote(elab.lvl),
+    };
+
+    show.build()
   }
 }
