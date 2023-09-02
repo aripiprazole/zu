dc9b64b924e1bc0064fe68d1c359a6ea88e8ae5c
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-09-02 20:18:49 +0000

    feat(elab): add check

--- src/passes/elab.rs
+++ src/passes/elab.rs
@@ -469,9 +469,23 @@ impl Elab {
 
   /// Checks a term against a type
   pub fn check(&self, term: &Tm, type_repr: Value) -> Expr {
-    let _ = term;
-    let _ = type_repr;
-    todo!()
+    match (term, type_repr) {
+      (Term::Hole(_), _) => self.fresh_meta(),
+
+      // Unifies the domain with the function parameter, and the codomain
+      // with it's body
+      (Term::Fun(ref fun), Value::Pi(_, _, box dom, cod)) => self
+        .create_new_value(&fun.arguments.text, dom)
+        .check(&fun.value, cod.apply(Value::rigid(self.lvl))),
+
+      // Fallback case that will cause an error if we can't check
+      // the term against the type.
+      (t, expected) => {
+        let inferred = self.infer(t);
+        self.unify(expected.clone(), inferred.clone());
+        t.clone().erase(self)
+      }
+    }
   }
 
   /// Performs unification between two values, its a
