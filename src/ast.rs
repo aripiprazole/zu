f247b76d29134404a14ff0102df298c8376cf9d2
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-09-05 07:21:52 +0000

    feat(elab): add shift function for definition

--- src/ast.rs
+++ src/ast.rs
@@ -81,6 +81,17 @@ pub struct Definition<S: state::State> {
   pub is_global: bool,
 }
 
+impl <M, FS: state::State<Meta = M>> Definition<FS> {
+  /// Converts the definition into a new definition with a different state.
+  pub fn shift<TS: state::State<Meta = M>>(self) -> Definition<TS> {
+    Definition {
+      text: self.text,
+      meta: self.meta,
+      is_global: self.is_global,
+    }
+  }
+}
+
 impl<S: state::State> Definition<S>
 where
   S::Meta: Default,
--- src/passes/elab/eval.rs
+++ src/passes/elab/eval.rs
@@ -19,26 +19,25 @@ impl Expr {
       Int(data) => Value::Int(data.value),
       Str(data) => Value::Str(data.value),
       Elim(_) => todo!("elim expr"),
-      Fun(e) => Value::Lam(Definition::new(e.arguments.text), Closure {
+      Fun(e) => Value::Lam(e.arguments.shift(), Closure {
         env: env.clone(),
         term: *e.value,
       }),
+      Anno(anno) => return anno.value.eval(env),
       Apply(e) => return e.callee.eval(env).apply(e.arguments.eval(env)),
       Reference(crate::quoting::Reference::Var(Ix(ix))) => return env.data[ix].clone(),
       Reference(crate::quoting::Reference::MetaVar(meta)) => match meta.take() {
         Some(value) => return value,
         None => return Type::flexible(meta),
       },
-      Anno(anno) => return anno.value.eval(env),
       Pi(pi) => {
-        let name = Definition::new(pi.domain.name.text);
         let domain = pi.domain.type_repr.eval(env);
         let codomain = Closure {
           env: env.clone(),
           term: *pi.codomain,
         };
 
-        Value::Pi(name, pi.domain.icit, domain.into(), codomain)
+        Value::Pi(pi.domain.name.shift(), pi.domain.icit, domain.into(), codomain)
       }
     })
   }
